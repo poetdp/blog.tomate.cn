c139fbc493827e36bdb97f18a35a89f2f4a3f5ce
rocj <rocj@qq.com>
2018-03-17 10:52:16 +0800

    install plugins

---
+++ install-plugins.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#deploy
+npm install hexo-deployer-git --save
+
+#RSS
+npm install hexo-generator-feed --save
+
+#qrcode
+npm install hexo-helper-qrcode --save
+
+#post置顶
+npm install hexo-helper-post-top --save
+
+#search
+npm install hexo-generator-search --save
+
+#highlight
+#npm install hexo-prism-plugin --save
+
+#sitemap
+npm install hexo-generator-sitemap --save
+
+#hexo-pdf
+npm install hexo-pdf --save
